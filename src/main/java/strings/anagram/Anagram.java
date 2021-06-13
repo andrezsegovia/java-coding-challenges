a3e5feb4d43f8013c65e0cae97b1edd0687947b5
Andrez S <ajsegoviacruz93@gmail.com>
2021-06-13 09:58:45 -0500

    using lambda functions to solve Anagram challenge

--- src/main/java/strings/anagram/Anagram.java
+++ src/main/java/strings/anagram/Anagram.java
@@ -1,35 +1,29 @@
 package strings.anagram;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Anagram {
     public int compute(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        Map<Character, Integer> aLetters = new HashMap<>();
-        Map<Character, Integer> bLetters = new HashMap<>();
-        int deletions = 0;
-        for(char c: a.toCharArray()){
-            aLetters.compute(Character.valueOf(c), (key, value) -> value == null? 1 : ++value);
-        }
-        for(char c: b.toCharArray()){
-            bLetters.compute(Character.valueOf(c), (key, value) -> value == null? 1 : ++value);
-        }
-        for(Character keys: aLetters.keySet()) {
-            int result = 0;
-            if(bLetters.containsKey(keys)) {
-                result = aLetters.get(keys) - bLetters.get(keys);
-                bLetters.remove(keys);
-            } else {
-                result = aLetters.get(keys);
-            }
-            deletions+=result<0?result*-1:result;
-        }
-        deletions+=bLetters.values().stream().reduce(0, Integer::sum);
+        Map<Character, Long> aLetters = a.chars()
+                .mapToObj(letter -> (char) letter)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        return deletions;
+        Map<Character, Long> bLetters = b.chars()
+                .mapToObj(letter -> (char) letter)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        BiFunction<Long, Long, Long> mergeDuplicatedKeys = (value1, value2) -> {
+            long diff = value1 - value2;
+            return diff < 0 ? diff * -1 : diff;
+        };
+
+        Map<Character, Long> mergedMap = new HashMap<>();
+        aLetters.forEach((key, value) -> mergedMap.merge(key, value, mergeDuplicatedKeys));
+        bLetters.forEach((key, value) -> mergedMap.merge(key, value, mergeDuplicatedKeys));
+        return mergedMap.values().stream().reduce(0L, Long::sum).intValue();
     }
 }

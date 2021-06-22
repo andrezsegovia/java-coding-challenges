aea45e27c6b47c7350061ca10d2c027dd4b5efb7
Andrez S <ajsegoviacruz93@gmail.com>
2021-06-22 09:57:14 -0500

    solution challenge003

--- src/main/java/Challenge003.java
+++ src/main/java/Challenge003.java
@@ -1,51 +1,17 @@
-import search.BinarySearch;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public class Challenge003 {
 
-    private static Map<Integer, Integer> getMapFromList(List<Integer> list) {
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        for(int i = 0; i < list.size(); i++) {
-            hashMap.put(i, list.get(i));
-        }
-        return hashMap;
-    }
-
-    private static Map<Integer, Integer[]> getMapFromArr(int[] arr) {
-        Map<Integer, Integer[]> hashMap = new HashMap<>();
-        for(int i = 0; i < arr.length; i++) {
-            hashMap.put(i, new Integer[]{arr[i],i});
-        }
-        return hashMap;
-    }
-
-
-
     public static int[] compute(int[] cost, int money) {
-        boolean found = false;
-        int index = 0;
-        Map<Integer, Integer[]> costMap = getMapFromArr(cost);
-        int value = 0;
-        int toFind = 0;
-        Integer foundIndex = 0;
-        while(!found && index < cost.length) {
-            value = cost[index];
-            toFind = money - value;
-            //foundIndex = BinarySearch.search(costMap, toFind);
-            if(foundIndex == null) {
-                index++;
-                continue;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < cost.length; i++) {
+            int complement = money - cost[i];
+            if(map.containsKey(complement)) {
+                return new int[]{map.get(complement), (i+1)};
             }
-            found = true;
+            map.put(cost[i],(i+1));
         }
-        if(found) {
-            return new int[]{index+1,foundIndex+1};
-        } else {
-            return new int[]{};
-        }
+        return new int[]{};
     }
 }

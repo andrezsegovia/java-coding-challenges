841ff04cfef10bef699948a974be0c54059cfdfd
Andrez S <ajsegoviacruz93@gmail.com>
2021-06-24 21:45:57 -0500

    complete challenge004

--- src/main/java/Challenge004.java
+++ src/main/java/Challenge004.java
@@ -1,5 +1,3 @@
-import com.sun.javaws.exceptions.InvalidArgumentException;
-
 import java.util.Arrays;
 
 public class Challenge004 {
@@ -9,14 +7,14 @@ public class Challenge004 {
      * @return {@code int} value that represents the median
      * @throws IllegalArgumentException when the array is null
      */
-    public int median(int[] arr) {
+    public double median(int[] arr) {
         if(arr.length == 0) {
             throw new IllegalArgumentException("array should not be empty");
         }
         if(arr.length % 2 == 0) {
             int middleLeft = arr[(arr.length/2)-1];
             int middleRight = arr[arr.length/2];
-            return (middleLeft+middleRight)/2;
+            return (middleLeft+middleRight)/2.0;
         }
         return arr[(arr.length/2)];
     }
@@ -59,5 +57,38 @@ public class Challenge004 {
             throw new IllegalArgumentException("array should not be empty");
         }
         sort(arr, 0, arr.length-1);
+    }
+
+    public boolean hasPreviousExpenditures(int[] expenditures, int trailingDays) {
+        if(expenditures.length == 0) {
+            throw new IllegalArgumentException("array should not be empty");
+        }
+        if(expenditures.length > trailingDays) {
+            return true;
+        }
+        return false;
+    }
+
+    public int notifications(int[] expenditures, int trailingDays) {
+        int notifications = 0;
+        int[] expendituresUnsorted = Arrays.copyOf(expenditures,expenditures.length);
+        sort(expenditures);
+        System.out.println(Arrays.toString(expendituresUnsorted));
+        System.out.println(Arrays.toString(expenditures));
+        if(!hasPreviousExpenditures(expenditures, trailingDays)) {
+            return 0;
+        }
+        for (int i = trailingDays; i < expenditures.length ; i++) {
+            int[] previousExpenditures = new int[trailingDays];
+            System.arraycopy(expenditures, i-trailingDays, previousExpenditures, 0, trailingDays);
+            System.out.println(Arrays.toString(previousExpenditures));
+            System.out.println(expendituresUnsorted[i]);
+            double median = median(previousExpenditures);
+            System.out.println(median*2);
+            if(expendituresUnsorted[i] >= (median*2)) {
+                notifications++;
+            }
+        }
+        return notifications;
     }
 }
--- src/test/java/Challenge004Test.java
+++ src/test/java/Challenge004Test.java
@@ -1,6 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class Challenge004Test {
@@ -25,30 +29,30 @@ public class Challenge004Test {
 
     @Test
     public void medianOfOddOrderedExpenditures() {
-        int expected = 30;
+        double expected = 30.0;
         int[] expenditures = {10,20,30,40,50};
-        assertEquals(expected, challenge004.median(expenditures));
+        assertEquals(expected, challenge004.median(expenditures),0);
     }
 
     @Test
     public void medianOfEvenOrderedExpenditures() {
-        int expected = 35;
+        double expected = 35.0;
         int[] expenditures = {10,20,30,40,50,60}; // 10 20 30 40 50 60 => (30+40)/2 = 35
-        assertEquals(expected, challenge004.median(expenditures));
+        assertEquals(expected, challenge004.median(expenditures),0);
     }
 
     @Test
     public void medianOfOddUnorderedExpenditures() {
-        int expected = 40;
+        double expected = 40.0;
         int[] expenditures = {20,30,40,10,50};
-        assertEquals(expected, challenge004.median(expenditures));
+        assertEquals(expected, challenge004.median(expenditures),0);
     }
 
     @Test
     public void medianOfEvenUnorderedExpenditures() {
-        int expected = 15;
+        double expected = 15.0;
         int[] expenditures = {50,30,20,10,40,60}; // 10 20 30 40 50 60 => (20+10)/2 = 15
-        assertEquals(expected, challenge004.median(expenditures));
+        assertEquals(expected, challenge004.median(expenditures), 0);
     }
 
     @Test
@@ -71,4 +75,99 @@ public class Challenge004Test {
         assertArrayEquals(expectedOrderedExpenditures, expenditures);
     }
 
+    @Test
+    public void hasPreviousExpendituresEmptyExpenditures() {
+        int[] expenditures = {};
+        int trailingDays = 4;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            challenge004.hasPreviousExpenditures(expenditures, trailingDays);
+        });
+        assertEquals("array should not be empty", exception.getMessage());
+    }
+
+    @Test
+    public void hasPreviousExpendituresSmallExpenditures() {
+        int[] expenditures = {10,20,30,40};
+        int trailingDays = 5;
+        assertEquals(false, challenge004.hasPreviousExpenditures(expenditures, trailingDays));
+    }
+
+    @Test
+    public void hasPreviousExpendituresEqualExpenditures() {
+        int[] expenditures = {10,20,30,40};
+        int trailingDays = 4;
+        assertEquals(false, challenge004.hasPreviousExpenditures(expenditures, trailingDays));
+    }
+
+    @Test
+    public void hasPreviousExpendituresEnoughExpenditures() {
+        int[] expenditures = {10,20,30,40,50};
+        int trailingDays = 4;
+        assertEquals(true, challenge004.hasPreviousExpenditures(expenditures, trailingDays));
+    }
+
+    @Test
+    public void calculateNotifications() {
+        int[] expenditures = {10,20,30,40,50};
+        int trailingDays = 3;
+
+        int notifications = challenge004.notifications(expenditures, trailingDays);
+
+        assertEquals(1, notifications);
+    }
+
+    @Test
+    public void calculateNotificationsWithoutEnoughExpenditures() {
+        int[] expenditures = {10,20,30,40};
+        int trailingDays = 4;
+
+        int notifications = challenge004.notifications(expenditures, trailingDays);
+
+        assertEquals(0, notifications);
+    }
+
+    @Test
+    public void calculateNotificationsLongerExpenditures() {
+        int[] expenditures = {10,20,30,40,50,70,80,90,100}; // 50
+        int trailingDays = 4;
+
+        int notifications = challenge004.notifications(expenditures, trailingDays);
+
+        assertEquals(2, notifications);
+    }
+
+    @Test
+    public void calculateNotifications_2() {
+        int[] expenditures = {2,3,4,2,3,6,8,4,5}; // 50
+        int trailingDays = 5;
+
+        int notifications = challenge004.notifications(expenditures, trailingDays);
+
+        assertEquals(2, notifications);
+    }
+
+    @Test
+    public void calculateNotifications_3() {
+        int[] expenditures = {1,2,3,4,4}; // 50
+        int trailingDays = 4;
+
+        int notifications = challenge004.notifications(expenditures, trailingDays);
+
+        assertEquals(0, notifications);
+    }
+
+    @Test
+    public void name() {
+        List<Integer> expences = new ArrayList<>();
+        expences.add(10);
+        expences.add(20);
+        expences.add(30);
+        expences.add(40);
+        expences.add(50);
+
+        int[] newArray = expences.stream().mapToInt(Integer::intValue).toArray();
+
+        System.out.println(Arrays.toString(newArray));
+
+    }
 }
